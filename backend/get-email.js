aa70611e76607ce256afe74c2e18782f48e3efcc
Your Name <your@email.com>
2026-03-11 18:38:37 +0530

    fix: improve bounce tracking and recipient extraction logic

--- backend/get-email.js
+++
@@ -1,9 +0,0 @@
-const prisma = require('./src/db/prismaClient');
-
-async function test() {
-    const user = await prisma.user.findFirst();
-    console.log('Valid email:', user.email);
-    process.exit(0);
-}
-
-test();
